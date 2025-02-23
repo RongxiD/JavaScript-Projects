6142cd97db6caf8cd3f52cee6e78c228ecf73a6a
RongxiD <drxxdu@gmail.com>
2025-02-22 23:52:11 -0800

    Escape Character

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -3,3 +3,5 @@ document.write(text);
 
 var myString = "HEY";
 window.alert(myString);
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br> \"Eat my shorts!\" Bart responded.");
