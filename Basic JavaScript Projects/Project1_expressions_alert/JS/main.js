9fecf93a1460b9f28be32da7c4f763512c20d092
RongxiD <drxxdu@gmail.com>
2025-02-22 23:53:56 -0800

    Concatenation

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,2 @@
 var text = "Hello, World!";
-document.write(text);
-
-var myString = "HEY";
-window.alert(myString);
-
-document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br> \"Eat my shorts!\" Bart responded.");
+document.write(text + " Guess Who I am ~");
